563b8c2c7408dc07a8ed83dd640c1e8e7b7b4117
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-02-21 22:15:49 -0800

    finished #7

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 //  # 7 Factory functions with LOTR
 
-const createCharacter = function (name, race, origin, attack, defense) {
+const createCharacter = function (name, race, origin, weapon, attack, defense) {
   return {
-    name: name,
+    nickname: name,
     race: race,
     origin: origin,
     attack: attack,
     defense: defense,
+    weapons: weapon,
     describe: function () {
-      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
+      console.log(`${this.nickname} is a ${this.race} from ${this.origin} who uses ${this.weapons}.`);
     },
     evaluateFight: function(character) {
       let x = this.attack - character.defense;
@@ -19,7 +20,33 @@ const createCharacter = function (name, race, origin, attack, defense) {
     
 };
 
-const test = createCharacter('gandalf','wizard','god',10,6);
-const test2 = createCharacter('gandalf2', 'wizard', 'god', 15, 1);
+let charArr = [];
 
-test.evaluateFight(test2);
+charArr.push(createCharacter('gandalf','Wizard','Middle Earth', 'wizard staff', 10, 6));
+charArr.push(createCharacter('bilbo','Hobbit','The Shire', 'Ring', 2, 1));
+charArr.push(createCharacter('frodo','Hobbit','The Shire', 'String and Barrow Blade',  3, 2));
+charArr.push(createCharacter('aragorn','Man','Dunnedain', 'Anduril',  6, 8));
+charArr.push(createCharacter('legolas','Elf','Woodland Realm', 'Bow and Arrow', 8, 5));
+
+charArr.push(createCharacter('Arwen Undomie', 'Half-Elf', 'Rivendell', 'Hadhafang', 10, 10));
+
+
+let found = charArr.find((element) => {
+  if(element.nickname === 'aragorn') {
+    return element.describe();
+  }
+});
+
+let hobbits = charArr.filter(char => {
+  if(char.race === 'Hobbit') {
+    return char;
+  }
+});
+
+let highAttack = charArr.filter(char => {
+  if(char.attack > 5) {
+    return char;
+  }
+});
+
+console.log(highAttack)
